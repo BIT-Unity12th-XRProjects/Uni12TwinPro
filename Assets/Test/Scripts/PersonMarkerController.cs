362c9972daaf1703030c35eeec6916e38e8cd11f
minsuhkim <124859537+minsuhkim@users.noreply.github.com>
2025-06-17 12:14:42 +0900

    QR 코드 인식 -> OCR AI 적용
    1. 이름표를 AR Camera로 인식하여 OCR AI를 통해 string으로 변환
    
    2. 해당 string이 유효하면 prefab생성

--- Assets/Test/Scripts/PersonMarkerController.cs
+++ Assets/Test/Scripts/PersonMarkerController.cs
@@ -1,6 +1,14 @@
 using TMPro;
 using UnityEngine;
 
+class StudentInfo
+{
+    public int id;
+    public string name;
+    public string birth;
+    // public string 
+}
+
 public class PersonMarkerController : MonoBehaviour
 {
     [SerializeField] private TextMeshPro _nameText;
